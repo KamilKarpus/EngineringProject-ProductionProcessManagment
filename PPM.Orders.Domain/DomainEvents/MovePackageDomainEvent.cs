69a0351601af028188ace02eaf83920a2d1f5719
Kamil Karpus <kamilkarpus97@gmail.com>
2020-08-18 20:41:52 +0200

    solution fixes

--- PPM.Orders.Domain/DomainEvents/MovePackageDomainEvent.cs
+++ PPM.Orders.Domain/DomainEvents/MovePackageDomainEvent.cs
@@ -1,8 +1,12 @@
 ﻿using PPM.Domain;
+using System;
 
 namespace PPM.Orders.Domain.DomainEvents
 {
     public class MovePackageDomainEvent : DomainEventBase
     {
+        public Guid OrderId { get; internal set; }
+        public Guid PackageId { get; internal set; }
+        public int Progress { get; internal set; }
     }
 }
